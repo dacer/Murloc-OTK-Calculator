3121f3d7087307fe4990900efc26f4857a941f9b
Dacer <qqwerrd@gmail.com>
2016-01-08 22:45:39 +0800

    init

---
+++ app/src/main/java/im/dacer/mrghllghghllghg/murloc/BaseMurloc.java
@@ -0,0 +1,12 @@
+package im.dacer.mrghllghghllghg.murloc;
+
+/**
+ * Created by Dacer on 1/8/16.
+ */
+public abstract class BaseMurloc {
+
+    public abstract int getAttack();
+    public abstract String getShownName();
+    public abstract int getBackgroundPic();
+
+}
---
+++ app/src/main/java/im/dacer/mrghllghghllghg/murloc/Grimscale.java
@@ -0,0 +1,7 @@
+package im.dacer.mrghllghghllghg.murloc;
+
+/**
+ * Created by Dacer on 1/8/16.
+ */
+public class Grimscale {
+}
